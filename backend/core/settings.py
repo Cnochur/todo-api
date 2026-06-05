f9de8bd9e06be1c921932c4201d10cc0ef442de1
cnochur <cnochursaunders@gmail.com>
2026-06-05 08:58:51 +0100

    Updating files after rename

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -40,7 +40,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'backend.urls'
+ROOT_URLCONF = 'core.urls'
 
 TEMPLATES = [
     {
@@ -57,7 +57,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'backend.wsgi.application'
+WSGI_APPLICATION = 'core.wsgi.application'
 
 
 # Database
