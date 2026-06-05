00ffbaaf69d61215eb1f8ff3424f8c08fe0745b5
cnochur <cnochursaunders@gmail.com>
2026-06-05 10:06:18 +0100

    web interface removed

--- backend/web/urls.py
+++
@@ -1,9 +0,0 @@
-from . import views
-from django.urls import path, include
-
-app_name = 'web'
-
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('tasks/', include('tasksAPI.urls')),
-]
---
+++ frontend/src/App.jsx
@@ -0,0 +1,6 @@
+import { useState, useEffect } from "react";
+import axios from "axios";
+import TodoList from "./components/TodoList";
+
+const API_URL = "http://127.0.0.1:8000/tasks"
+}
